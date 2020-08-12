b0bd6f0cbc2333835f396a9c78b66fcc5d717e10
GlennTan <e0310963@u.nus.edu>
2020-08-12 17:20:11 +0800

    # changes to CSS for H1 under mastheader.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
     */
-    (function ($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
+            this.pathname.replace(/^\//, "") &&
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
@@ -54,3 +54,15 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+$(document).ready(function () {
+    // Typed.JS
+    $(function () {
+        $(".TypedJs").typed({
+            strings: ["a Computing Student","a Web Developer", "a Tech Enthusiast"],
+            typeSpeed: 15,
+            loop: true,
+            backDelay: 2500
+        });
+    });
+});
